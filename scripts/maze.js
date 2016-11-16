352d789df94de14f96b5baaa687794cf454faf5a
Andy Wolff <andy.wolff@gmail.com>
2016-11-15 19:22:58 -0600

    second, just some more structure for a maze

---
+++ scripts/maze.js
@@ -0,0 +1,11 @@
+"use strict";
+console.log('maze');
+function Maze(width, height) {
+  this.width = width;
+  this.height = height;
+  this.startX = null;
+  this.startY = null;
+  this.starOrientation = null;
+  this.endX = null;
+  this.endY = null;
+}
