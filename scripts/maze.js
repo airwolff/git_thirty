733c72821ad6ae8875c88a748483ad60427f83d7
Andy Wolff <andy.wolff@gmail.com>
2016-11-16 22:53:52 -0600

    building a maze

--- scripts/maze.js
+++ scripts/maze.js
@@ -1,15 +1,29 @@
 "use strict";
 console.log('maze');
+
 function Maze(width, height) {
-  this.width = width;
-  this.height = height;
-  this.startX = null;
-  this.startY = null;
-  this.starOrientation = null;
-  this.endX = null;
-  this.endY = null;
+	this.width = width;
+	this.height = height;
+	this.startX = null;
+	this.startY = null;
+	this.starOrientation = null;
+	this.endX = null;
+	this.endY = null;
 }
 
-Maze.prototype.setStart = function() {
-  
+this.directions = ['north', 'east', 'south', 'west'];
+this.spaces = [];
+
+var x, y;
+for (x = 1; x <= width; x += 1) {
+	this.spaces[x] = [];
+	for (y = 1; y <= height; y += 1) {
+		this.spaces[x][y] = new MazeSpace(this.directions);
+	}
+}
+
+Maze.prototype.setStart = function (x, y, orientation) {
+	this.startX = x;
+	this.startY = y;
+	this.startOrientation = orientation;
 }
